e01dc683a639a43ccc0a41d89cca1086a260e532
micheal <micheal@gmail.com>
2023-04-25 12:36:23 +0100

    commit

--- item_lister/pratice.js
+++ item_lister/pratice.js
@@ -1,55 +1,60 @@
 let form = document.querySelector("#form"),
-  item_list = document.querySelector("#items"),
+  item_lists = document.querySelector("#items"),
   filter = document.querySelector("#filter");
 
+// add event
 form.addEventListener("submit", add_items);
-item_list.addEventListener("click", remove_item);
+item_lists.addEventListener("click", remove_item);
 filter.addEventListener("keyup", filter_items);
+
+// add items function
 function add_items(e) {
   e.preventDefault();
-  // new items
+  // new list item
   let new_item = document.querySelector("#item");
-  //check if the new item input is empty
-  if (new_item.value != "") {
-    // create li element
-    let li = document.createElement("li");
-    // add class name to li
-    li.className = "list-group-item";
-    // append new item to li
-    li.appendChild(document.createTextNode(new_item.value));
-    // append li to item list
-    item_list.appendChild(li);
-    // create delete btn
-    delete_btn = document.createElement("button");
-    // add class name to delete btn
-    delete_btn.className = "btn btn-danger btn-sm float-right delete";
-    // create text next for delete btn
-    delete_btn.appendChild(document.createTextNode("X"));
-    // append delete btn to li
-    li.appendChild(delete_btn);
-  }
+  //create li element
+  let li = document.createElement("li");
+  //add class name to element
+  li.className = "list-group-item";
+  //append child and create text node
+  li.appendChild(document.createTextNode(new_item.value));
+  // create delete button element
+  let delete_btn = document.createElement("button");
+  // create class name for delete btn
+  delete_btn.className = "btn btn-danger btn-sm float-right delete";
+  //append and create text node to delete btn
+  delete_btn.appendChild(document.createTextNode("X"));
+  //append delete btn to li
+  li.appendChild(delete_btn);
+  //append li to item lists
+  item_lists.appendChild(li);
 }
+// remove items function
 function remove_item(e) {
+  // if the event target class name contain class of delete
   if (e.target.classList.contains("delete")) {
-    if (confirm("Do you want to delete this item?")) {
+    // confirm if the user want to delete the item
+    if (confirm("Do you want to really delete this item?")) {
+      // get the parent element of the delete class
       li = e.target.parentElement;
-      item_list.removeChild(li);
+      // removing the child node form the parent node
+      item_lists.removeChild(li);
     }
   }
 }
+
 function filter_items(e) {
-  // console.log(e.target.value.toLowerCase());
+  // convert the even target value to lowercase and assign it to a variable
   let text = e.target.value.toLowerCase();
-  // get all li element by tag name assign it to items;
-  let items = item_list.getElementsByTagName("li");
-  //covert the li html collection to array
-  Array.from(items).forEach((item) => {
-    // assign the content from item to item_name
-    let item_name = item.firstChild.textContent;
+  let items = item_lists.getElementsByTagName("li");
+  //convert html collection to array
+  Array.from(items).forEach((value) => {
+    let item_name = value.firstChild.textContent;
     if (item_name.toLowerCase().indexOf(text) != -1) {
-      item.style.display = "block";
+      value.style.display = "block";
     } else {
-      item.style.display = "none";
+      value.style.display = "none";
     }
   });
+  // console.log(items);
 }
