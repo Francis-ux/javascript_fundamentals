a8870e236dfb9784410abd95072307442423ef35
micheal <micheal@gmail.com>
2023-04-29 17:39:36 +0100

    commit

---
+++ oop/inheritance.js
@@ -0,0 +1,31 @@
+function Books(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  //   this.getSummary = function () {
+  //     return `${this.title} was written by ${this.author} in the year ${this.year}`;
+  //   };
+}
+
+//get summary
+Books.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in the year ${this.year}`;
+};
+
+//Magazine Constructor
+
+function Magazine(title, author, year, month) {
+  Books.call(this, title, author, year);
+  this.month = month;
+}
+
+//Inherit Prototype
+//Instantiate Magazine object
+// const mag1 = new Magazine("Mag 1", "john Mike", "2008", "june");
+// console.log(mag1.getSummary());
+Magazine.prototype = Object.create(Books.prototype);
+const mag2 = new Magazine("Mag 2", "Mike Jackson", "2008", "june");
+
+//Use Magazine Constructor
+// Magazine.prototype.constructor = Magazine;
+console.log(mag2);
